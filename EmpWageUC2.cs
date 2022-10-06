3af3e78c7dc46411aa1be471c781d76ad235e38c
Deekshita1996 <113499307+Deekshita1996@users.noreply.github.com>
2022-10-06 21:56:06 +0530

    Emp Wage Calculation
    Emp Wage Calculation

---
+++ EmpWageUC2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class EmpWageUC2
+    {
+        public static void EmpDayEarn()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int emphrs = 0;
+            int empwage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                emphrs = 8;
+            }
+            else
+            {
+                emphrs = 0;
+            }
+            empwage = emphrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Daily Emp Wage is :" + empwage);
+
+            Console.ReadKey();
+        }
+    }
+
+}
